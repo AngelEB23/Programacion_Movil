75cd25503e313a12712745679d98024547d19828
AngelEB23 <jaeblol239@gmail.com>
2022-07-04 10:52:44 -0500

    -

---
+++ Corte2/RecycleViewDB/app/src/main/java/com/example/recycleview/Aplicacion.java
@@ -0,0 +1,35 @@
+package com.example.recycleview;
+
+import android.app.Application;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class Aplicacion extends Application {
+    private ArrayList<Alumno> alumnos;
+    private MiAdaptador adaptador;
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        alumnos = Alumno.llenarAlumnos();
+        adaptador = new MiAdaptador(alumnos, this);
+        Log.d("", "onCreate: tamaño array list " + alumnos.size());
+    }
+
+    public ArrayList<Alumno> getAlumnos() {
+        return alumnos;
+    }
+
+    public void setAlumnos(ArrayList<Alumno> alumnos) {
+        this.alumnos = alumnos;
+    }
+
+    public MiAdaptador getAdaptador() {
+        return adaptador;
+    }
+
+    public void setAdaptador(MiAdaptador adaptador) {
+        this.adaptador = adaptador;
+    }
+}
